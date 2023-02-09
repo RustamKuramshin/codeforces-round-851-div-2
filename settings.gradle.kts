d1a8e4339bf0fbb7d8d6619c8abd494ce1d815b9
Rustam Kuramshin <leo.in.empty@gmail.com>
2023-02-09 16:48:09 +0300

    commit

---
+++ settings.gradle.kts
@@ -0,0 +1,2 @@
+rootProject.name = "codeforces-round-851-div-2"
+
---
+++ src/main/java/Task3.java
@@ -0,0 +1,57 @@
+import java.io.BufferedOutputStream;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
+
+public class Task3 {
+
+    public static PrintWriter out;
+
+    public static class Reader {
+
+        static BufferedReader reader;
+        static StringTokenizer tokenizer;
+
+        static void init(InputStream input) {
+            reader = new BufferedReader(new InputStreamReader(input));
+            tokenizer = new StringTokenizer("");
+        }
+
+        static String next() throws IOException {
+            while ( ! tokenizer.hasMoreTokens() ) {
+                tokenizer = new StringTokenizer(reader.readLine());
+            }
+            return tokenizer.nextToken();
+        }
+
+        static int nextInt() throws IOException {
+            return Integer.parseInt(next());
+        }
+
+        static double nextDouble() throws IOException {
+            return Double.parseDouble(next());
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        Reader.init(System.in);
+        out = new PrintWriter(new BufferedOutputStream(System.out));
+
+        // Далее идет ваш код
+
+        // Пример чтения строк, целых и вещественных чисел
+        Reader.next();
+        Reader.nextInt();
+        Reader.nextDouble();
+
+        // Пишем в ответ
+        out.println("Test");
+
+
+        // Отправить накопленный вывод на stdout
+        out.close();
+    }
+}
